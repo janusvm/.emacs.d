b40d7715830e7883a4169e730602af769823ace7
Janus Valberg-Madsen <janus@math.aau.dk>
2018-10-16 11:43:21 +0200

    Add dwim setting to dired

--- settings/setup-dired.el
+++ settings/setup-dired.el
@@ -1,6 +1,9 @@
 ;; setup-dired.el : settings related to dired
 ;; -----------------------------------------------------------------------------
 
+;; Copy/move between dired windows
+(setq dired-dwim-target t)
+
 ;; Use a button for opening file/folder in same buffer
 (put 'dired-find-alternate-file 'disabled nil)
 
